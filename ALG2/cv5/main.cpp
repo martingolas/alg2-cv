274560d81a7f16ae277d8f1b9b8d0fe360f236e5
martingolas <martingolasowski@gmail.com>
2015-03-19 10:28:06 +0100

    Oprava

--- ALG2/cv5/main.cpp
+++ ALG2/cv5/main.cpp
@@ -5,15 +5,6 @@ void main()
 {
 	Graph *g = new Graph(8);
 
-	g->AddEdge(1, 2);
-	g->AddEdge(1, 3);
-	g->AddEdge(2, 4);
-	g->AddEdge(2, 5);
-	g->AddEdge(3, 6);
-	g->AddEdge(3, 7);
-	g->AddEdge(5, 8);
-
-	g->TraverseBFS(1);
 
 	std::cin.get();
 }
